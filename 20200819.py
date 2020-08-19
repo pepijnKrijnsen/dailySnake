d22ed95754a7466032585fe0b1a61e3403cd374c
Pepijn Krijnsen <69592786+pepijnKrijnsen@users.noreply.github.com>
2020-08-19 13:25:11 +0100

    Complete 20200819.py

--- 20200819.py
+++ 20200819.py
@@ -1,8 +1,10 @@
 # THE OS MODULE
 # 
-# Python can use the os module to use OS specific methods and get OS specific attributes. The # module is built in to Python and does not need to be installed - you only need to import it:
+# Python can use the os module to use OS specific methods and get OS specific attributes. The 
+# module is built in to Python and does not need to be installed - you only need to import it:
 import os
-# A full description of the module including all methods and attributes can be found in the Python # docs: https://docs.python.org/3/library/os.html
+# A full description of the module including all methods and attributes can be found in the Python 
+# docs: https://docs.python.org/3/library/os.html
 # The OS module is required for several common automation actions, including (but not limited to):
 # 1. Getting details about the OS
 # 2. Renaming, moving, or deleting files & directories
@@ -13,10 +15,15 @@ import os
 # Note that copying or creating files (not directories) can be handled with Python's built-in 
 # open() function.
 # 
-# One more feature that is useful for certain automation tasks is to give Python access to # arguments passed when calling the script. In other words, if I have a script called backup.py # that copies any file provided to it to the ~/backups directory, you can use the sys.argv() # method.
+# One more feature that is useful for certain automation tasks is to give Python access to 
+# arguments passed when calling the script. In other words, if I have a script called backup.py 
+# that copies any file provided to it to the ~/backups directory, you can use the sys.argv() 
+# method.
 # This method is part of the sys module, so first we need to...
 import sys
-# Now we can access any arguments passed to our Python script through sys.argv(). This method # returns a list where index0 is the name of the script itself and any subsequent indices are # arguments passed to the script.
+# Now we can access any arguments passed to our Python script through sys.argv(). This method 
+# returns a list where index0 is the name of the script itself and any subsequent indices are 
+# arguments passed to the script.
 # For example: if I want to backup my photo.jpg using this script, I can call
 backup.py photo.jpg
 # ...and the list returned by sys.argv() now contains:
