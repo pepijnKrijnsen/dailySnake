f2db1c7bd0f0bc6fae51d222580b40f5ba2855fb
Pepijn Krijnsen <69592786+pepijnKrijnsen@users.noreply.github.com>
2020-08-22 21:13:59 +0100

    Create 20200822.py

---
+++ 20200822.py
@@ -0,0 +1,5 @@
+# RECAP WEEK 1
+# 
+# This week we started by looking at what 'things' can be passed to functions as arguments. Turns out: pretty much anything you like, including calls to other functions (in which case those functions will be resolved and their return values used when resolving the 'calling' function).
+# We also looked at recursion and investigated the difference between applying a certain change to a list in two different ways: using a for loop (iteration) and using recursion. Recursion is an interesting topic, mostly applicable to functional languages (or rather, functional programming - which can be done to some degree in Python) and we will certainly be looking into it again.
+# The rest of the week was spent looking closely at various methods related to creating, reading, writing, and appending to files. This topic had less to do with the logic of programming and more to do with practical applications of basic Python scripts. I think I have exhausted the open() function and will be looking at a different practical example next week.
